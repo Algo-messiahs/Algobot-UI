712dc2314b4b13dba64d5acfb639a68a3393e99e
Kyle <kyle.weidner14@protonmail.com>
2021-10-19 19:49:35 -0600

    Modify login route

--- algobot/urls.py
+++ algobot/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import login_required
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'algobot'
 urlpatterns = [
     path('', views.index, name='index'),
     # path('login/', views.login, name='login'),
-    path(r'login/$', login_required,{'template_name': 'account/login.html'} )
+    # path(r'login/$', login_required, {'template_name': 'account/login.html'}),
 ]
