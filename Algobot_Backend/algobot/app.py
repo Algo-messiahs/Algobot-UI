9631e506a815ec44449b9659f0f82a6e77d716f5
Eesha <eesha@Eeshas-MacBook-Pro.local>
2021-11-03 15:34:26 -0600

    adding chalice config

---
+++ Algobot_Backend/algobot/app.py
@@ -0,0 +1,35 @@
+from chalice import Chalice
+import requests, json
+
+app = Chalice(app_name='algobot')
+
+#API_KEY =
+#SECRET_KEY =
+#BASE_URL =
+#ORDERS_URL =
+#HEADERS =
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
