2af04f36be28ad323640a07ee833185b12b58ecd
Kyle <kyle.weidner14@protonmail.com>
2021-12-14 19:56:24 -0700

    Add Heroku to ALLOWED_HOSTS

--- Algobot_Django/settings.py
+++ Algobot_Django/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-@5evh1uk*s!^k9c*)b+ou#t17+#ye4q#bxjdpc#@5$1vrek=^0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evening-everglades-82303.herokuapp.com']
 
 
 # Application definition
