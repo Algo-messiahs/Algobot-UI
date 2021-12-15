fd4e4fcf5f0618360b259547060b0b3b918663bf
Kyle <kyle.weidner14@protonmail.com>
2021-12-14 21:29:18 -0700

    edit settings.py

--- Algobot_Django/settings.py
+++ Algobot_Django/settings.py
@@ -100,7 +100,8 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # baltlogs.com
 
